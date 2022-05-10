14f3de2950f4205a2856098c83d2b2afe0eba115
cmoyano <cristianmoyano.mza@gmail.com>
2022-05-10 14:51:28 -0300

    Initial version

---
+++ tasks.py
@@ -0,0 +1,8 @@
+from invoke_release.tasks import *  # noqa: F403
+
+
+configure_release_parameters(  # noqa: F405
+    module_name="jarvis",
+    display_name="PyJarvis",
+    use_pull_request=True,
+)
