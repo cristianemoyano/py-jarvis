7c2d152969d8323ba0a50b74c03e9290a393011d
cmoyano <cristianmoyano.mza@gmail.com>
2022-05-12 11:43:29 -0300

    [MINOR] Add strategy

--- jarvis/speak/speak.py
+++ jarvis/speak/speak.py
@@ -1,11 +1,11 @@
 import logging
+from abc import ABC, abstractmethod
 from time import sleep
 
 import pyttsx3
 from gtts import gTTS
 from playsound import playsound
 from utils.logger import setup_logger
-from utils.singleton import SingletonMeta
 
 logger = logging.getLogger(__name__)
 setup_logger(logger)
@@ -13,11 +13,54 @@ setup_logger(logger)
 
 GTTS_ENGINE = "gtts"
 PYTTSX3_ENGINE = "pyttsx3"
-SPEECH_ENGINE_DEFAULT = GTTS_ENGINE
+SPEECH_ENGINE_DEFAULT = PYTTSX3_ENGINE
 
 
-class TextToSpeach(metaclass=SingletonMeta):
+class SpeakStrategy(ABC):
+    """
+    The Strategy interface declares operations common to all supported versions
+    of some algorithm.
 
+    The Context uses this interface to call the algorithm defined by Concrete
+    Strategies.
+    """
+
+    @abstractmethod
+    def speak(self, message):
+        pass
+
+
+class SpeakAPI:
+    """
+    The SpeakAPI defines the interface of interest to clients.
+    """
+
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy):
+        """
+        SpeakAPI allows replacing a Strategy object at runtime.
+        """
+        self._strategy = strategy
+
+    def speak(self, message):
+        self._strategy.speak(message)
+
+
+class GTTSEngineStrategy(SpeakStrategy):
+    def speak(self, message):
+        audio = gTTS(message, tld="com", lang="en")
+        audio.save("textaudio.mp3")
+        playsound("textaudio.mp3")
+
+
+class Pyttsx3EngineStrategy(SpeakStrategy):
     _data = {}
 
     RATE = 162
@@ -44,31 +87,30 @@ class TextToSpeach(metaclass=SingletonMeta):
             )  # changing index, changes voices. 1 for female
             cls._data["engine"] = engine
 
+    def speak(self, message):
+        if not self.engine()._inLoop:
+            self.engine().say(message)
+            logger.info("Not in loop..run and wait")
+            self.engine().runAndWait()
+            sleep(2)
+            self.engine().endLoop()
+        else:
+            logger.info("in loop..stop, run and wait")
+            self.engine().endLoop()
+            self.engine().say(message)
+            self.engine().runAndWait()
+            sleep(2)
+            self.engine().endLoop()
+
+
+def get_speak_strategy_instance(strategy_code):
+    SPEECH_ENGINE_MAP = {
+        GTTS_ENGINE: GTTSEngineStrategy,
+        PYTTSX3_ENGINE: Pyttsx3EngineStrategy,
+    }
+    return SPEECH_ENGINE_MAP[strategy_code]()
+
 
 def speak(message):
-    if SPEECH_ENGINE_DEFAULT == GTTS_ENGINE:
-        speak_gtts(message)
-    else:
-        speak_pyttsx3(TextToSpeach.engine(), message)
-
-
-def speak_gtts(message):
-    audio = gTTS(message, tld="com", lang="en")
-    audio.save("textaudio.mp3")
-    playsound("textaudio.mp3")
-
-
-def speak_pyttsx3(engine, message):
-    if not engine._inLoop:
-        engine.say(message)
-        logger.info("Not in loop..run and wait")
-        engine.runAndWait()
-        sleep(2)
-        engine.endLoop()
-    else:
-        logger.info("in loop..stop, run and wait")
-        engine.endLoop()
-        engine.say(message)
-        engine.runAndWait()
-        sleep(2)
-        engine.endLoop()
+    api = SpeakAPI(get_speak_strategy_instance(SPEECH_ENGINE_DEFAULT))
+    api.speak(message)
